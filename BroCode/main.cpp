e247607a3d475feac8553a14656e9902859e914c
manasikasande <61113000+manasikasande@users.noreply.github.com>
2020-03-28 19:21:02 +0000

    Update main.cpp

--- BroCode/main.cpp
+++ BroCode/main.cpp
@@ -1,3 +1,4 @@
+#include "user.h"
 #include <iostream>
 using namespace std;
 int main(int argc, char const *argv[])
@@ -6,29 +7,129 @@ int main(int argc, char const *argv[])
 	cout<<"\n\tSelect the type of user: ";
 	cout<<"\n\t1. Librarian";
 	cout<<"\n\t2. Student";
-	cout<<"\n\t3. Teacher";
+	cout<<"\n\t3. Teacher"<<endl;
 	int ch = 0;
-	cin>>ch
-
+	cin>>ch;
+	cin.get();
+	user u;	
 	switch(ch)
 	{
 		case 1: //Librarian
 		{
 			cout<<"\n\t1. Log in";
-			cout<<"\n\t2. Sign up";
+			cout<<"\n\t2. Sign up"<<endl;
 			int ch1 = 0;
 			cin>>ch1;
+			cin.get();
+			switch(ch1)
+			{
+				case 1:		// log in
+				{
+					string email,password;
+					do
+					{
+						cout<<"\n\tEnter your email"<<endl;
+						getline(cin,email);
+						cout<<"\n\tEnter the password"<<endl;
+						getline(cin,password);
+
+						if(u.login(email,password))
+							cout<<"\n\tLogin Successful";
+						else
+							cout<<"\n\tWrong email id/password. Try Again";
+					}while(!u.login(email,password));
+				}
+				break;
+
+				case 2:		// sign up
+				{
+
+				}				
+			
+			}	
+		}
+		break;
+
+		case 2: //Student
+		{
+			cout<<"\n\t1. Log in";
+			cout<<"\n\t2. Sign up"<<endl;
+			int ch1 = 0;
+			cin>>ch1;
+			cin.get();
 
 			switch(ch1)
 			{
-				string email,password;
-				cout<<"\n\tEnter your email";
-				getline(email,cin);
-				cout<<"\n\tEnter the password";
-				getline(password,cin);
-				
-			}	
+				case 1:		// log in
+				{
+					string email,password;
+					do
+					{
+						cout<<"\n\tEnter your email"<<endl;
+						getline(cin,email);
+						cout<<"\n\tEnter the password"<<endl;
+						getline(cin,password);
+
+						if(u.login(email,password))
+							cout<<"\n\tLogin Successful";
+						else
+							cout<<"\n\tWrong email id/password. Try Again";
+					}while(!u.login(email,password));
+				}
+				break;
+
+				case 2:		// sign up
+				{
+
+				}
+				break;					
+			}
 		}
+		break;
+
+		case 3: //Teacher
+		{
+			cout<<"\n\t1. Log in";
+			cout<<"\n\t2. Sign up"<<endl;
+			int ch1 = 0;
+			cin>>ch1;
+			cin.get();
+
+			switch(ch1)
+			{
+				case 1:		// log in
+				{
+					string email,password;
+
+					do
+					{
+						cout<<"\n\tEnter your email"<<endl;
+						getline(cin,email);
+						cout<<"\n\tEnter the password"<<endl;
+						getline(cin,password);
+
+						if(u.login(email,password))
+							cout<<"\n\tLogin Successful";
+						else
+							cout<<"\n\tWrong email id/password. Try Again";
+					}while(!u.login(email,password));
+					
+
+
+				}
+				break;
+
+				case 2:		// sign up
+				{
+
+				}
+				break;					
+			}
+		}
+		break;
+
+		default:
+		cout<<"\n\tWrong input.Try Again !"<<endl;
 	}
 
 	return 0;
