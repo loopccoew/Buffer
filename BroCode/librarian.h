d6fa60b77689a5ab504e6b8433966790f84d125f
AaryaD <62545207+AaryaD@users.noreply.github.com>
2020-03-29 17:51:58 +0530

    addBooks and updateBooks added

---
+++ BroCode/librarian.h
@@ -0,0 +1,18 @@
+#include<iostream>
+#include<string>
+
+using std::string;
+class librarian
+{
+	private:
+
+	string bookname;
+	string author;
+	string bookno;
+	string num;
+	
+	public:
+		    librarian();
+			void addBook();
+			void updateBooks();
+};
