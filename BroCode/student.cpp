09ad7b3c594e742675d17bb5bcb481830044ab48
manasikasande <61113000+manasikasande@users.noreply.github.com>
2020-03-30 18:50:05 +0000

    Update student.cpp

--- BroCode/student.cpp
+++ BroCode/student.cpp
@@ -1,86 +1,80 @@
-#include <cmath>
-#include <cstdio>
-#include <vector>
-#include <iostream>
-#include <algorithm>
-#include <string>
+#include "student.h"
 using namespace std;
-class student: public person
-{
-	private:
 
-	string dept;
-	string cnum;
-	string emailid;
-	string name;
-	int year;
-
-	public:
-
-	student()
+	student::student()
 	{
 		dept=" ";
 		cnum=" ";
-		emailid=" ";
-		name=" ";
 		year=0;
 	}	
 
-	void input()
+	void student::input()
 	{
 		fstream fout;
 		fout.open("library.csv",ios::out | ios::app);
-		cout<<"\nEnter details:";
-		cout<<"\nEnter dept:";
-		cin>>dept;
-		cout<<"\nEnter cnum:";
-		cin>>cnum;
-		cout<<"\nEnter year:";
+		cout<<"\n\tEnter your details:";
+		person::input();		
+		cout<<"\n\tEnter dept:";
+		getline(cin,dept);
+		cout<<"\n\tEnter cnum:";
+		getline(cin,cnum);
+		cout<<"\n\tEnter year:";
 		cin>>year;
-		cout<<"\nEnter emailid:";
-		cin>>emailid;
-		cout<<"\nEnter name:";
-		cin>>name;
-		fout<<cnum<<","<<emailid<<","<<name<<","<<dept<<","<<year<<","<<"\n";
+
+		fout<<cnum<<","
+			<<email<<","
+			<<name<<","
+			<<dept<<","
+			<<year<<"\n";
+
+		fout.close();
 	}
 
-	void show(string emailid)
+	void student::show(string emailid)
 	{
+		
 		fstream fout;
 		fout.open("library.csv", ios::in);
 		vector<string> row;
-		string line,word,temp;
-		int flag=0;
-		while(fout>temp)
+		string line,word;
+		int flag = 0;	// 0 indicates record not found
+
+		while(fout)
 		{
+
 			row.clear();
 			getline(fout,line); //stores an entire row in line variable
 			stringstream s(line); //breaks into words
-			while(getline(s,word,","))
+			while(getline(s,word,','))
 			{
 				row.push_back(word);
 			}
-			if(emailid== row[1])
+
+			if(emailid == row[1])
 			{
-				flag=1;
-				cout<<"Student Details:";
-				cout<<"Emailid"<<row[1]<<"\n";
-				cout<<"C Number:"<<row[0]<<"\n";
-				cout<<"Name:"<<row[2]<<"\n";
-				cout<<"Department:"<<row[3]<<"\n";
-				cout<<"Year:"<<row[4]<<"\n";
+
+				flag = 1;	//record found
+				cout<<"\n\tStudent Details : "<<endl;
+				cout<<"\n\tEmail id : "<<row[1];
+				cout<<"\n\tC Number : "<<row[0];
+				cout<<"\n\tName : "<<row[2];
+				cout<<"\n\tDepartment : "<<row[3];
+				cout<<"\n\tYear : "<<row[4]<<endl;
 				break;
 			}
 
 		}
-		if(flag==0)
+
+		if(flag == 0)
 		{
-			cout<<"\nRecord not found!";
+			cout<<"\n\tRecord not found!";
 		}
+
+		fout.close();
 	}
 
+	string student::getEmail()
+	{
+		return email;
+	}
 
-
-
-
-};
