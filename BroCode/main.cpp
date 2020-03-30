a372486981296b98b862c92e8e7c4838c503f99d
manasikasande <61113000+manasikasande@users.noreply.github.com>
2020-03-30 18:52:42 +0000

    Update main.cpp

--- BroCode/main.cpp
+++ BroCode/main.cpp
@@ -1,3 +1,5 @@
+#include "librarian.h"
+#include "student.h"
 #include "user.h"
 #include <iostream>
 using namespace std;
@@ -37,7 +39,57 @@ int main(int argc, char const *argv[])
 							cout<<"\n\tLogin Successful";
 						else
 							cout<<"\n\tWrong email id/password. Try Again";
-					}while(!u.login(email,password));
+					}while(u.login(email,password) == false);
+
+					int ch2 = 0;
+
+					do
+					{
+
+						
+						cout<<"\n\tWelcome, "<<email<<" !";
+						cout<<"\n\tOPTIONS ";
+						cout<<"\n\t1. Add Books"
+							<<"\n\t2. Update a book"
+							<<"\n\t3. Remove a book"
+							<<"\n\t4. Search for a specific user"
+							<<"\n\t5. Display all users"
+							<<"\n\t6. Exit"<<endl;
+						
+						librarian l;
+						cin>>ch2;
+
+						switch(ch2)
+						{
+							case 1:
+							l.addBook();
+							break;
+
+							case 2:
+							l.updateBooks();
+							break;
+
+							case 3:
+							cout<<"\n\tFunction yet to be written";
+							break;
+
+							case 4:
+							cout<<"\n\tFunction yet to be written";
+							break;
+
+							case 5:
+							cout<<"\n\tFunction yet to be written";
+							break;
+
+							case 6:
+							cout<<"\n\tExiting..";
+							break;
+
+							default:
+							cout<<"\n\tPlease enter correct option";
+						}
+
+					}while(ch2 != 6);	
 				}
 				break;
 
@@ -74,13 +126,19 @@ int main(int argc, char const *argv[])
 							cout<<"\n\tLogin Successful";
 						else
 							cout<<"\n\tWrong email id/password. Try Again";
-					}while(!u.login(email,password));
+					}while(u.login(email,password) == false);
 				}
 				break;
 
 				case 2:		// sign up
 				{
-
+					student s;
+					string password;
+					s.input();
+					cin.get();
+					cout<<"\n\tEnter a password";
+					getline(cin,password);
+					u.saveLogin(s.getEmail(),password);
 				}
 				break;					
 			}
@@ -112,7 +170,7 @@ int main(int argc, char const *argv[])
 							cout<<"\n\tLogin Successful";
 						else
 							cout<<"\n\tWrong email id/password. Try Again";
-					}while(!u.login(email,password));
+					}while(u.login(email,password) == false);
 					
 
 
