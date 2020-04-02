11cfc43022c3def1221ceee11d2c098aaac8f8cf
AaryaD <62545207+AaryaD@users.noreply.github.com>
2020-04-02 11:43:49 +0530

    Update librarian.cpp

--- BroCode/librarian.cpp
+++ BroCode/librarian.cpp
@@ -67,9 +67,9 @@ using namespace std;
         stringstream s(line); 
   
         while(getline(s,word,','))
-		{ 
+         { 
             row.push_back(word); 
-        } 
+         } 
   
         //strcpy(name1,name); cannot use strcpy for string
         name1 = name;    
@@ -77,7 +77,7 @@ using namespace std;
   
         if((name.compare(name1)) == 0) 
         {
-        	count = 1; 
+            count = 1; 
             stringstream convert;
 		
   
@@ -88,9 +88,10 @@ using namespace std;
             row[index] = convert.str(); 
   
             if (!fin.eof()) 
-			{ 
+	    { 
                 for (i = 0; i < row_size - 1; i++)
-				 { 
+				 
+		{ 
   
                     // write the updated data 
                     // into a new file 'booksnew.csv' 
@@ -102,11 +103,11 @@ using namespace std;
             } 
         } 
         else 
-		{ 
+	{ 
             if (!fin.eof())
-			 { 
+	     { 
                 for (i = 0; i < row_size - 1; i++) 
-				{ 
+		{ 
   
                     // writing other existing records 
                     // into the new file using fout. 
@@ -132,3 +133,79 @@ using namespace std;
     // renaming the updated file with the existing file name 
     rename("booksnew.csv", "books.csv"); 
   } 
+void librarian::deleteBooks() 
+{ 
+  
+    // Open FIle pointers 
+    fstream fin, fout; 
+  
+    // Open the existing file 
+    fin.open("books.csv", ios::in); 
+  
+    // Create a new file to store the non-deleted data 
+    fout.open("booksnew.csv", ios::out); 
+  
+    int rollnum, roll1, marks, count = 0, i; 
+    string name,name1;
+    int index, new_marks; 
+    string line, word; 
+    vector<string> row; 
+  
+    // Get the roll number 
+    // to decide the data to be deleted 
+    cout << "Enter the name of the book to be removed: "; 
+   getline(cin,name);
+  
+    // Check if this record exists 
+    while (!fin.eof())
+	 { 
+  
+        row.clear(); 
+        getline(fin, line); 
+        stringstream s(line); 
+  
+        while (getline(s, word, ', ')) 
+	{ 
+            row.push_back(word); 
+        } 
+  
+        int row_size = row.size(); 
+        name1=name;
+  
+        // writing all records, 
+        // except the record to be deleted, 
+        // into the new file 'booksnew.csv' 
+        // using fout pointer 
+         if((name.compare(name1)) != 0) 
+	  { 
+            if (!fin.eof())
+	    { 
+                for (i = 0; i < row_size - 1; i++) 
+		{ 
+                    fout << row[i] << ", "; 
+                } 
+                fout << row[row_size - 1] << "\n"; 
+            } 
+        } 
+        else 
+	{ 
+            count = 1; 
+        } 
+        if (fin.eof()) 
+            break; 
+    } 
+    if (count == 1) 
+        cout << "Record deleted\n"; 
+    else
+        cout << "Record not found\n"; 
+  
+    // Close the pointers 
+    fin.close(); 
+    fout.close(); 
+  
+    // removing the existing file 
+    remove("books.csv"); 
+  
+    // renaming the new file with the existing file name 
+    rename("booksnew.csv", "books.csv"); 
+}  
