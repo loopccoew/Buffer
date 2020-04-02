2b8866895f3918fa3d66d11ff552be6eede1a8fe
manasikasande <61113000+manasikasande@users.noreply.github.com>
2020-04-02 19:19:07 +0000

    Update user.cpp

--- BroCode/user.cpp
+++ BroCode/user.cpp
@@ -1,8 +1,9 @@
 #include "user.h"
 using namespace std;
 	
-	bool user::login(string email,string password)
+	bool user::login(string email,string password,char type)
 	{
+		email.push_back(type);
 		//hashing the password using sha256
 		string hash = picosha2::hash256_hex_string(password);
 
@@ -55,7 +56,7 @@ using namespace std;
 
 	}
 
-	void user::saveLogin(string email,string password)
+	void user::saveLogin(string email,string password,char type)
 	{
 		//hashing the password using sha256
 		string hash = picosha2::hash256_hex_string(password);
@@ -73,10 +74,65 @@ using namespace std;
 
 		//write the email and password to file
 		out<<email;
+		out<<type;
 		out<<":";
 		out<<hash;
 		out<<"\n";
 
 		out.close();	
+	}
+
+	bool user::isUnique(string email,char type)
+	{
+
+		/* This function checks whether there is an existing user with 
+			the email id 'email'
+			Returns false if present
+			true if not i.e email id is unique
+		*/
+			
+		email.push_back(type);
+
+		//open the file in which all the email,passwords are stored
+		//we have to read the file here
+		//hence creating an object of ifstream
+		ifstream in("password.txt",ios::in|ios::binary);
+		map<string,string> hashmap; 	//hashmap to store data in email,password format
+		string line;
+		vector<string> keyvalue;
+		while(in)
+		{
+			in>>line;
+			boost::split(keyvalue,line,boost::is_any_of(":"));
+			hashmap.insert(pair<string,string>(keyvalue[0],keyvalue[1]));
+
+		}
+
+		map<string,string>::iterator it;
+
+		/* 
+			Display hashmap for debugging purpose
+
+			for(it = hashmap.begin(); it != hashmap.end(); it++)
+			{
+				cout<<it->first<<endl;
+				cout<<it->second<<endl;
+			} 
+		*/
+
+		it = hashmap.find(email);	//search for the key
+		if(it == hashmap.end())
+		{
+			//key not found
+			//A user with this email id does not exist";
+			return true;
+		}
+		else
+		{
+			//key found
+			return false;
+		}
+
+		in.close();
 	}
 
