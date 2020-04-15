e59f8c28a823d89f5a0f8ffe311e38d3558c03ac
samruddhi812 <61051512+samruddhi812@users.noreply.github.com>
2020-04-15 17:06:25 +0530

    Update att.java

--- att.java
+++ att.java
@@ -1,32 +1,33 @@
-package p;
-import java.lang.* ; 
+package loop;
+
+import java.awt.*;
 import java.awt.event.*;
 import javax.swing.*;
 import javafx.scene.control.PasswordField;
 import java.util.*;
-class node      // Class node for entities of students
+class node      
 {
  node right,left;
  String name;
  int rollno;
  int attend;
 }
-class bst      // Class bst to implement Binary search tree
+class bst
 {
- node root;		// Root of the tree of type node
+ node root;
  node H[] = new node[50];
  node H1[] = new node[50];
 String[] a = new String[10];
   int no;
   int cnt;
- bst() // default constructor 
+ bst()
  {
   root =null;
   no = 0;
   cnt=0;
  }
  
- void insert(String n,int r,int at)		// Function definition to insert students' deatils in a Binary search tree
+ void insert(String n,int r,int at)
  {
   node prev,ptr,cur;
   int flag=0;
@@ -78,7 +79,7 @@ String[] a = new String[10];
   }
  
  }
- node search(String n,int r)// Function definition to search for a student and return it
+ node search(String n,int r)
  {
   node cur;
   cur=root;
@@ -107,7 +108,7 @@ String[] a = new String[10];
    return null;
   }
  }
- void inorder(node localroot)// Function Definition for Recursive Inorder Traversal
+ void inorder(node localroot)//Recursive Inorder Traversal
  {
   if(localroot!=null)
   {
@@ -116,7 +117,7 @@ String[] a = new String[10];
    inorder(localroot.right);
   }
  }
- node update(String nm,int r)// Function definition to update attendance of a student and return the updated value
+ node update(String nm,int r)
  {
   node cur;
   cur=root;
@@ -148,7 +149,7 @@ String[] a = new String[10];
  }
  
 
-void downadjustment(int i) // Function definition for down adjacement in heap
+void downadjustment(int i)
 {
 int smallest =i;
 int l=2*i;
@@ -157,7 +158,7 @@ if(l<=no && H[l].attend<H[i].attend)
 {
 smallest = l;
 }
-if(r<=no && H[r].attend<H[smallest].attend)
+if(r<=no && H[r].attend<H[smallest].attend) 
 {
 smallest = r;
 }
@@ -170,7 +171,7 @@ downadjustment(smallest);
 }
 }
 
-void BuildHeap()   // Function definition to build a heap
+void BuildHeap()   
 {
 int i=0;
 for(i=no/2;i>0;i--)
@@ -180,14 +181,14 @@ downadjustment(i);
 for(int j=1;j<=no;j++)
 {
 H1[j]=H[j];
-if(H1[j].attend<1)    //Minimum attendance is considered 1 here as a sample case but can be set to 75% of the complete attendance in practical case
+if(H1[j].attend<1)
 {
 delete();
 }
 }
 }
  
-void delete()  // Function Definition to delete entry of a particular student
+void delete()
 {
 if(H[2*1] == null && H[2*1+1] == null)
 {
@@ -211,7 +212,7 @@ H1[i]=H[i];
 }
 }
 
-void display()		 // Function Definition to display the DEFAULTER'S LIST
+void display()
 {
 System.out.println("\n\t Students in defaulters list : ");
 for(int i=0;i<cnt;i++)
@@ -222,24 +223,19 @@ System.out.println(a[i]);
 }
 
 
-class att implements ActionListener// Class for Graphics section
+class att implements ActionListener
 {
- JFrame f1;//  Frame
+ JFrame f1;
  JFrame f2;
  JFrame f3;
  JFrame f4;
  JFrame f5;
- JFrame f6 ; 
- JFrame f7 ; 
- JFrame f8 ; 
- JFrame f9 ; 
- JFrame f10 ; 
- JPasswordField pf1;// Password Field 
- JTextArea ta , ta1 , ta2  ;
- JPanel p1 ,p2,p3,p4,p5,p6,p7,p8, p9 , p10 ;
- JTextField t1,t2,t3,t4,t5,t6, t7;
- JButton b1,b2,b3,b4,b5,b6,b7,b8,b9,b10,b11 , b12 ;
- JLabel l1,l2,l3,l4,l5,l6 , l7 ;
+ JPasswordField pf1;
+ JTextArea ta;
+ JPanel p1 ,p2,p3,p4,p5;
+ JTextField t1,t2,t3,t4,t5,t6;
+ JButton b1,b2,b3,b4,b5,b6,b7,b8;
+ JLabel l1,l2,l3,l4,l5,l6;
  int x=1000;
  int code=0;
  att()
@@ -253,73 +249,27 @@ class att implements ActionListener// Class for Graphics section
   f3= new JFrame("TEACHERS");
   f3.setBounds(50,50,450,450);
   f3.setLayout(null);
-  f6= new JFrame("TEACHERS");
-  f6.setBounds(50,50,450,450);
-  f6.setLayout(null);
   f4= new JFrame("STUDENT");
   f4.setBounds(50,50,450,450);
   f4.setLayout(null);
-  
   f5= new JFrame("STUDENT LIST");
   f5.setBounds(50,50,450,450);
   f5.setLayout(null);
-  
-  f7= new JFrame("STUDENT");
-  f7.setBounds(50,50,450,450);
-  f7.setLayout(null);
-  
-  f8= new JFrame("TEACHERS");
-  f8.setBounds(50,50,450,450);
-  f8.setLayout(null);
-  
-  f9= new JFrame("DEFAULTER LIST ");
-  f9.setBounds(50,50,450,450);
-  f9.setLayout(null);
-  
-  f10= new JFrame("DEFAULTER LIST ");
-  f10.setBounds(50,50,450,450);
-  f10.setLayout(null);
-  
   p1 = (JPanel)f1.getContentPane();
   p1.setLayout(null);
   p1.setBounds(50,50,550,550);
-  
   p2 = (JPanel)f2.getContentPane();
   p2.setLayout(null);
   p2.setBounds(50,50,550,550);
-  
   p3 = (JPanel)f3.getContentPane();
   p3.setLayout(null);
   p3.setBounds(50,50,550,550);
-  
   p4 = (JPanel)f4.getContentPane();
   p4.setLayout(null);
   p4.setBounds(50,50,550,550);
-  
   p5 = (JPanel)f5.getContentPane();
   p5.setLayout(null);
   p5.setBounds(50,50,550,550);
-  
-  p6 = (JPanel)f6.getContentPane();
-  p6.setLayout(null);
-  p6.setBounds(50,50,550,550);
-  
-  p8 = (JPanel)f8.getContentPane();
-  p8.setLayout(null);
-  p8.setBounds(50,50,550,550);
-  
-  p7 = (JPanel)f7.getContentPane();
-  p7.setLayout(null);
-  p7.setBounds(50,50,550,550);
-  
-  p9 = (JPanel)f9.getContentPane();
-  p9.setLayout(null);
-  p9.setBounds(50,50,550,550);
-  
-  p10 = (JPanel)f10.getContentPane();
-  p10.setLayout(null);
-  p10.setBounds(50,50,550,550);
-  
   b1=new JButton("TEACHERS");
   b1.setBounds(50, 150,100,100);
   b2=new JButton("STUDENT");
@@ -334,21 +284,8 @@ class att implements ActionListener// Class for Graphics section
   b6.setBounds(200, 50, 200, 50);
   b7=new JButton("LOG OUT");
   b7.setBounds(200, 350, 100, 50);
-  b8=new JButton("MAKE DEFAULTER LIST");
-  b8.setBounds(200,250,200, 50);
-  
-  b9=new JButton("GIVE ATTENDANCE ");
-  b9.setBounds(200,100,150, 50);
-  
-  b10=new JButton("TAKE ATTENDANCE ");
-  b10.setBounds(200,100,150, 50);
-  
-  b11=new JButton("SHOW DEFAULTER LIST ");
-  b11.setBounds(200,250,200, 50);
-  
-  b12=new JButton("DISPLAY DEFAULTER LIST ");
-  b12.setBounds(200,300,200, 50);
-  
+  b8=new JButton("show list");
+  b8.setBounds(200,250,100, 50);
   b1.addActionListener(this);
   b2.addActionListener(this);
   b3.addActionListener(this);
@@ -357,45 +294,22 @@ class att implements ActionListener// Class for Graphics section
   b6.addActionListener(this);
   b7.addActionListener(this);
   b8.addActionListener(this);
-  b9.addActionListener(this);
-  b10.addActionListener(this);
-  b11.addActionListener(this);
-  b12.addActionListener(this);
-
-  
-  
   l1=new JLabel("username");
   l1.setBounds(50, 50, 150, 50);
-  
   l2=new JLabel("password");
   l2.setBounds(50, 150, 150, 50);
-  
   l3=new JLabel("Name");
   l3.setBounds(50, 50, 150, 50);
-  
   l4=new JLabel("Rollno");
   l4.setBounds(50, 150, 150, 50);
-  
   l5=new JLabel("Enter code");
   l5.setBounds(50, 250, 150, 50);
-  
   l6=new JLabel("CODE");
   l6.setBounds(50, 300, 150, 50);
-  
-  l7=new JLabel("Enter no of mendatory lectures for defaulters ");
-  l7.setBounds(50, 50, 300, 50);
-  
-  t7=new JTextField(40);
-  t7.setBounds(100, 100, 150, 50);
-  
   t6=new JTextField(40);
   t6.setBounds(300,300,100,25);
-  
   t4=new JTextField(40);
   t4.setBounds(300, 50, 150, 50);
-  
- 
-  
   t5=new JTextField(40);
   t5.setBounds(200,150,200,50);
   t1=new JTextField(40);
@@ -408,39 +322,18 @@ class att implements ActionListener// Class for Graphics section
   pf1.setBounds(300, 150, 150, 50);
   ta=new JTextArea();
   ta.setBounds(50, 50,400 , 400);
-  
-  ta=new JTextArea();
-  ta.setBounds(50, 50,400 , 400);
- 
-  ta1=new JTextArea();
-  ta1.setBounds(50, 50,400 , 400);
-  
-  ta2=new JTextArea();
-  ta2.setBounds(50, 50,400 , 400);
-  
   p1.add(b1);
   p1.add(b2);
-  
- 
-  
   p2.add(b3);
   p2.add(b4);
   p2.add(l1);
   p2.add(l2);
   p2.add(t4);
   p2.add(pf1);
-  
-  p6.add(b10) ; 
-  p6.add(b8) ; 
-  p6.add(b7) ; 
-  
   p3.add(b6);
   p3.add(b7);
   p3.add(t5);
-  
-  p7.add(b9) ; 
-  p7.add(b11) ; 
-  
+  p3.add(b8);
   p4.add(b5);
   p4.add(l3);
   p4.add(l4);
@@ -450,51 +343,33 @@ class att implements ActionListener// Class for Graphics section
   p4.add(t3);
   //p4.add(l6);
   //p4.add(t6);
-  p8.add(l7) ; 
-  p8.add(t7) ; 
-  p8.add(b12) ; 
-  
   p5.add(ta);
-  p9.add(ta1) ;
-  p10.add(ta2) ; 
-  
   f1.setVisible(true);
  }
  public static void main(String args[])
  {
-  
   new att();
 
  }
- int flag = 0 ;
- int h = 0 ; 
- node a[] = new node[10] ; 
  @SuppressWarnings("unlikely-arg-type")
  @Override
  public void actionPerformed(ActionEvent e) {
   // TODO Auto-generated method stub
   bst b =new bst();
-  
-  
-  
   b.insert("samruddhi", 3,0);
   b.insert("vaibhavi",2 ,0);
   b.insert("sakshi", 1,0);
   b.insert("mayuri", 4,0);
-  b.insert("sakshi", 5,0);
-  b.insert("shruti",6,0);
+  //b.insert("sakshi", 5,0);
+  b.insert("shruti",5,0);
   if(e.getSource()==b1)
   {
    f2.setVisible(true);
   }
   else if(e.getSource()==b2)
   {
-   f7.setVisible(true);
+   f4.setVisible(true);
    //f2.setVisible(true);
-  }
-  else if(e.getSource() == b9 )
-  {
-	  f4.setVisible(true);
   }
   else if(e.getSource()==b3)
   {
@@ -515,29 +390,12 @@ class att implements ActionListener// Class for Graphics section
    // System.out.println(""+i);
    if(user.equals(f)&&i==corpass.length)
    {
-	   
-	   //f3.setVisible(true);
-	   f6.setVisible(true);
-	   /*
-	   if(e.getSource()==b10)
-	   {
-		   f3.setVisible(true);
-	   }
-	   */
+    f3.setVisible(true);
    }
    else
    {
     JOptionPane.showMessageDialog(null,"invalid username or password");
    }
-  }
-  else if(e.getSource() == b10 )
-  {
-	  f3.setVisible(true);
-  }
-  else if(e.getSource() == b8 )
-  {
-	   f8.setVisible(true);
-	   
   }
   else if(e.getSource()==b4)
   {
@@ -573,13 +431,22 @@ class att implements ActionListener// Class for Graphics section
      System.out.println("\n\t"+ptr.name+"\t"+ptr.rollno+"\t"+ptr.attend);
      ptr=ptr.right;
     }while(s.empty()==false || ptr!=null);
+    
+    b.BuildHeap();
+    b.display();
+    
+    ta.append("\n\tdefaulter list is :");
+    for(int j=0;j<b.cnt;j++)
+    {
+    ta.append("\n\t"+b.a[j]);
+    }
+
    }
    else
    {
     JOptionPane.showMessageDialog(null,"roll number or code is invalid");
    }
-b.BuildHeap();
-b.display();
+
   }
   else if(e.getSource()==b6)
   {
@@ -595,61 +462,5 @@ b.display();
    f5.setVisible(true);
 
   }
-  else if(e.getSource() == b11 )
-  {
-	  f10.setVisible(true);
-	  if(h==0 )
-	  {
-		  ta2.append("list is not displayed yet " );
-	  }
-	  else
-	  {
-	  for(int  i = 0 ; i<a.length ; i++ )
-	  {
-		  ta2.append("ROLL NO " + a[i].rollno + a[i].name + "\n");
-	  }
-	  }
-  }
-  else if(e.getSource() == b12 )
-  {
-	  f8.setVisible(false);
-	  f9.setVisible(true);
-	   String x = t7.getText() ; 
-	   int xn = Integer.parseInt(t7.getText()) ; 
-	   
-	   
-	   ta1.append("DEFAULTER LIST DISPLAYED ");
-	   node ptr;
-	    Stack<node> s=new Stack<node>();
-	    //node ptr;
-	    ptr=b.root;
-	    ta1.append("DEFAULTER LIST " + "\n");
-	    do
-	    {
-	     while(ptr!=null)
-	     {
-	      s.push(ptr);
-	      ptr=ptr.left;
-	     }
-	     ptr=s.pop();
-	     //System.out.println(ptr.data+" ");
-	     if(ptr.attend < xn )
-	     {
-	    	 a[h] = new node() ; 
-	    	 a[h] = ptr ; 
-	    	 
-	    	 ta1.append("ROLL NO " + ptr.rollno + ptr.name + "\n") ; 
-	    	 h++ ; 
-	    	 flag = 1 ;
-	     }
-	     ptr=ptr.right;
-	    }while(s.empty()==false || ptr!=null);
-	    if(flag == 0 )
-	    {
-	    	ta1.append("NO DEFAULTERS ");
-	    }
-  }
  }
 }
-
-
